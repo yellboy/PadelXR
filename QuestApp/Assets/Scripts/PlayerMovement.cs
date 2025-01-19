7197824fae67ea2ae10f68f4513db9abe5af2bf4
bojan.jelaca@zuehlke.com <Bojan.Jelaca@zuehlke.com>
2025-01-19 14:31:49 +0100

    Added player movement

---
+++ QuestApp/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private const float Speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var thumbstick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+
+        print("thumbstick: " + thumbstick);
+
+        var movementRight = thumbstick.x * Speed * Time.deltaTime;
+        var movementForward = thumbstick.y * Speed * Time.deltaTime;
+
+        gameObject.transform.position += gameObject.transform.forward * movementForward +
+                                          gameObject.transform.right * movementRight;
+    }
+}
