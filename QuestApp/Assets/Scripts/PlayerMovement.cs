f251287d5691a3fccd2cc39fff8631886f614eab
bojan.jelaca@zuehlke.com <Bojan.Jelaca@zuehlke.com>
2025-01-25 12:41:13 +0100

    Adjusted movement

--- QuestApp/Assets/Scripts/PlayerMovement.cs
+++ QuestApp/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
 
+using Assets.Scripts.Utilities;
 using UnityEngine;
+using UnityEngine.UIElements;
 using Vector3 = UnityEngine.Vector3;
 
 public class PlayerMovement : MonoBehaviour
@@ -17,12 +19,12 @@ public class PlayerMovement : MonoBehaviour
     {
         var thumbstick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
 
-        print("thumbstick: " + thumbstick);
-
         var movementRight = thumbstick.x * Speed * Time.deltaTime;
         var movementForward = thumbstick.y * Speed * Time.deltaTime;
 
-        gameObject.transform.position += Camera.main.transform.forward * movementForward +
-                                          Camera.main.transform.right * movementRight;
+        var flatForward = Camera.main.transform.forward.Flattened().normalized;
+        var flatRight = Camera.main.transform.right.Flattened().normalized;
+
+        gameObject.transform.position += flatForward * movementForward + flatRight * movementRight;
     }
 }
---
+++ QuestApp/Assets/Scripts/Utilities/Vector3Extensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Utilities
+{
+    public static class Vector3Extensions
+    {
+        public static Vector3 Flattened(this Vector3 vector)
+        {
+            return new Vector3(vector.x, 0, vector.z);
+        }
+    }
+
+}
